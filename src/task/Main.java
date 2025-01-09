9e90fd15dab1d45608edb61fbe9d85f2864c7cd0
Aibek <clesh.meder.271@gmail.com>
2025-01-09 12:10:17 +0600

    tekshergeni

--- src/task/Main.java
+++ src/task/Main.java
@@ -1,5 +1,9 @@
+package task;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("i'm Aibek");
     }
+
 }
---
+++ src/task/models/Lesson.java
@@ -0,0 +1,4 @@
+package task.models;
+
+public class Lesson {
+}
